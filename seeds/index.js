c4a602ad777c736ccca27c9505be06591cf74477
tiztrain <adamtolo20@hotmail.com>
2023-05-15 19:37:39 +0200

    first commit 	new file:   .gitignore 	new file:   app.js 	new file:   models/campground.js 	new file:   package-lock.json 	new file:   package.json 	new file:   seeds/cities.js 	new file:   seeds/index.js 	new file:   seeds/seedHelpers.js 	new file:   views/campgrounds/edit.ejs 	new file:   views/campgrounds/index.ejs 	new file:   views/campgrounds/show.ejs 	new file:   views/home.ejs 	new file:   views/layouts/boilerplate.ejs 	new file:   views/partials/footer.ejs 	new file:   views/partials/navbar.ejs

---
+++ seeds/index.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
+const Campground = require("../models/campground");
+
+mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random()*20)+10;
+    const camp = new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: 'https://source.unsplash.com/collection/8870517',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint temporibus officiis exercitationem voluptatum iste consequatur corrupti necessitatibus ut quam omnis nesciunt consectetur mollitia molestias sequi, asperiores et aspernatur ipsam deleniti.',
+      price
+    });
+    await camp.save();
+  }
+};
+
+seedDB().then(() => {
+   mongoose.connection.close(); 
+})
